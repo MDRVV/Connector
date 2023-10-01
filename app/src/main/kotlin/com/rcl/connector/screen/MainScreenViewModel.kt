d21fcd8b37c5b5e3680a035cd530cba9209fe5dd
Lucker <Samir-ramazanov-1995@mail.ru>
2023-10-02 01:47:22 +0300

    Add a list of streets and method for it

--- app/src/main/kotlin/com/rcl/connector/screen/MainScreenViewModel.kt
+++ app/src/main/kotlin/com/rcl/connector/screen/MainScreenViewModel.kt
@@ -1,7 +1,24 @@
 package com.rcl.connector.screen
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rcl.connector.KoinObj.koin
+import com.rcl.connector.koin.ktor.KtorRepository
+import com.rcl.connector.model.HouseModel
+import com.rcl.connector.model.StreetModel
+import kotlinx.coroutines.launch
 
 class MainScreenViewModel: ViewModel() {
 
+    val streetList = mutableStateListOf<StreetModel>()
+    init {
+        getStreetList()
+    }
+    private fun getStreetList(){
+        viewModelScope.launch {
+            streetList.addAll(koin.get<KtorRepository>().getStreetList())
+        }
+    }
 }
