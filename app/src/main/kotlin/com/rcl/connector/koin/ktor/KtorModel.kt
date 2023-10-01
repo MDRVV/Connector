746537c3900133459eedef2a5ca569a4ff54070e
Lucker <Samir-ramazanov-1995@mail.ru>
2023-10-01 23:44:34 +0300

    Remove ktor negotation by default (because server can't give values with application/json)

--- app/src/main/kotlin/com/rcl/connector/koin/ktor/KtorModel.kt
+++ app/src/main/kotlin/com/rcl/connector/koin/ktor/KtorModel.kt
@@ -2,10 +2,7 @@ package com.rcl.connector.koin.ktor
 
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
-import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 object KtorModel {
@@ -13,13 +10,6 @@ object KtorModel {
         single {
             HttpClient(OkHttp) {
                 expectSuccess = true
-                install(ContentNegotiation) {
-                    json(Json {
-                        isLenient = true
-                        ignoreUnknownKeys = true
-                        prettyPrint = true
-                    })
-                }
                 install(Logging){
                     logger = Logger.DEFAULT
                     level = LogLevel.ALL
--- app/src/main/kotlin/com/rcl/connector/koin/ktor/KtorRepository.kt
+++ app/src/main/kotlin/com/rcl/connector/koin/ktor/KtorRepository.kt
@@ -1,16 +1,22 @@
 package com.rcl.connector.koin.ktor
 
+import com.rcl.connector.model.HouseModel
 import com.rcl.connector.model.StreetModel
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
+import kotlinx.serialization.json.Json
 
 class KtorRepository(private val httpClient: HttpClient) {
     private val baseUrl = "https://stat-api.airnet.ru/v2/utils/get"
 
-
-    suspend fun getStreetsList(): List<StreetModel> {
+    suspend fun getStreetList(): List<StreetModel> {
         val url = "${baseUrl}/allStreets/"
-        return httpClient.get(url).body()
+        return Json.decodeFromString<List<StreetModel>>(httpClient.get(url).body())
+    }
+    suspend fun getHouseList(streedId: String): List<HouseModel> {
+        var url = "${baseUrl}/houses/"
+        url+="?street_id=${streedId}"
+        return Json.decodeFromString<List<HouseModel>>(httpClient.get(url).body())
     }
 }
