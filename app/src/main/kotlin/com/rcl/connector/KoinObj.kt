f6d58513e2ae8c04107e4078b461dce182aa8e7d
Lucker <Samir-ramazanov-1995@mail.ru>
2023-10-01 23:45:46 +0300

    Object for koin(to not rewrite it on full recomposition)

---
+++ app/src/main/kotlin/com/rcl/connector/KoinObj.kt
@@ -0,0 +1,8 @@
+package com.rcl.connector
+
+import org.koin.core.Koin
+
+object KoinObj {
+    lateinit var koin: Koin
+    var inited = false
+}
--- app/src/main/kotlin/com/rcl/connector/MainActivity.kt
+++ app/src/main/kotlin/com/rcl/connector/MainActivity.kt
@@ -9,13 +9,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
+import com.rcl.connector.KoinObj.inited
+import com.rcl.connector.KoinObj.koin
+import com.rcl.connector.koin.ktor.KtorModel
 import com.rcl.connector.screen.MainScreen
 import com.rcl.connector.ui.theme.Theme.ConnectorTheme
+import org.koin.core.context.startKoin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        if (!inited) {
+            koin = startKoin { modules(KtorModel.networkModule) }.koin
+            inited = true
+        }
         setContent {
             ConnectorTheme {
                 Surface(
