723b21dd33bdad1b3bad38e82a055444abd1ee34
Lucker <Samir-ramazanov-1995@mail.ru>
2023-10-02 22:01:36 +0300

    Add minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             signingConfig = signingConfigs.getByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
